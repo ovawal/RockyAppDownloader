92d2921ad39b3112a718685a4f25b7abb77ed276
Walter <wangakiokova@gmail.com>
2025-03-14 10:47:11 -0500

    Added comments and improved prompt

--- AppDownloader.sh
+++ AppDownloader.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-#
-#
-read -p "Which app(s) would you like to download?" -a applications
-#
-#
+
+
+# Prompts the user for apps and stores it array
+read -p "Which app(s) would you like to download? " -a applications
+
+
+# Checks if any input was provided
 if [ ${#applications[@]} -eq 0  ]; then
 	echo "No application entered."
 	exit 1
 fi
-#
-#
+
+# Downloads each application in the array if it exists 
 for app in "${applications[@]}";
 	do
 		if [ -n "$app" ]; then
@@ -24,5 +26,3 @@ for app in "${applications[@]}";
 		fi
 	done
 exit 0
-
-
